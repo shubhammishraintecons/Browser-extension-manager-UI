6d7cdcc1a842944ce59a97e67d6821de25419bf4
Shubham Mishra <shubham.mishra@intecons.com>
2025-07-16 18:49:42 +0530

    theme-color-mode

--- src/component/Navbar/Headerbar.jsx
+++ src/component/Navbar/Headerbar.jsx
@@ -5,8 +5,29 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import LogoIcons from './LogoIcons';
+import Iconmoon from '../../assets/images/icon-moon.svg'
+import Iconsun from '../../assets/images/icon-sun.svg'
+import { useEffect, useState } from 'react';
 
 function Headerbar() {
+  const [darkMode, setDarkMode] = useState(true)
+
+
+  // applied the useEffect
+  useEffect(() => {
+    // Apply/remove class from <body>
+    if (darkMode) {
+      document.body.classList.add('dark-mode')
+      document.body.classList.remove('light-mode')
+    }
+    else {
+      document.body.classList.add('light-mode')
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkMode])
+
+  const toggleTheme = () => setDarkMode(!darkMode);
+
   return (
     <Container className='header-nav-margin'>
       <Navbar expand="lg" className="bg-body-dark">
@@ -19,8 +40,12 @@ function Headerbar() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Form className="">
-              <div className='icons-theme'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#FBFDFE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z" /></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z" /></clipPath></defs></svg>
+              <div className='icons-theme' onClick={toggleTheme} style={{ cursor: 'pointer' }}>
+                {darkMode ? (
+                  <img src={Iconmoon} alt="Moon Icon" width={22} height={22} />
+                ) : (
+                  <img src={Iconsun} alt="Sun Icon" width={22} height={22} />
+                )}
               </div>
             </Form>
           </Navbar.Collapse>
